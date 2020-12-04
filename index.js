b70e91d2e8c2dbfc2c44b7550bcbf5426965371b
Olorea <firchovaa@gmail.com>
2020-12-04 23:07:06 +0100

    Add canvas with graph rendring and message

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
-let i = 0;
-setInterval(()=>{
-    document.body.innerHTML = (i++%2?`X`:'O');
-},600);
+
+
+const canvas = document.querySelector('canvas');
+
+console.log(canvas);
+
+const ctx = canvas.getContext('2d');
+
+console.log(ctx);
+
+ctx.fillStyle = '#ff0000';
+ctx.lineWidth=2;
+ctx.strokeStyle='#00ff00';
+
+/*
+ctx.fillRect(10,10,300,30);
+ctx.strokeRect(10,10,300,30);
+*/
+
+ctx.beginPath();
+
+ctx.moveTo(250,250);
+for(let i=-100;i<100;i+=0.01){
+    ctx.lineTo(
+        Math.sin(i)*i*10+250,
+        Math.cos(i)*i*10+250
+    );
+}
+ctx.stroke();
+
+
+ctx.font = "30px Arial";
+ctx.strokeText('ahoj',30,30,40);
+
