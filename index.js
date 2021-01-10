b059ed323bd6f3d251bf3142b3782944d84e33e1
Olorea <firchovaa@gmail.com>
2021-01-10 01:23:19 +0100

    Flock of stars

--- index.js
+++ index.js
@@ -116,15 +116,19 @@ alert(4);
 
 
 let lastTime=performance.now();
-let ball0 = {x:0, y:0};
-let ball1x=50,ball1y=50;
-let ball2x=100,ball2y=100;
-let ball3x=150,ball3y=150;
-let ball4x=200,ball4y=200;
-let ball5x=300,ball5y=300;
 
-let balls = [{x:250, y:250,color:'red'},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250, color: 'blue'}];
+let balls = [];//[{x:40, y:100, height: 60, width: 60, color:'red'},{x:250, y:250, size: 66},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:250, y:250},{x:220, y:220, color: 'yellow' },{x:250, y:250},{x:250, y:250, color: 'blue'}];
 
+for(let i=0;i<100;i=i+1){
+    balls.push({
+        x: Math.random()*500,
+        y: Math.random()*500,
+        edges: 2.5,//2+Math.ceil(Math.random()*18),
+        rotation: Math.random()*360,
+        size: 5+Math.random()*10,
+        color:'#'+Math.floor(Math.random()*16777215).toString(16)
+    });
+}
 
 
 canvas.addEventListener('pointerup',(event)=>{
@@ -140,81 +144,14 @@ const drawBall = ()=>{
   
     ctx.clearRect(0,0,500,500);
 
-    
-    ctx.beginPath();
-    ctx.strokeStyle = "blue"
-    for(let i= -Math.PI;i<=Math.PI;i+=0.03){
-        ctx.lineTo(
-            Math.cos(i)*30+ball0.x,
-            Math.sin(i)*30+ball0.y,
-        );
-    }
-    ctx.stroke();
-
-    ball0.x=ball0.x-10*duration;
-    ball0.y=ball0.y-7*duration;
-
-
-    ctx.beginPath();
-    ctx.strokeStyle = "red"
-    for(let i= -Math.PI;i<=Math.PI;i+=0.03){
-        ctx.lineTo(
-            Math.sin(i)*30+ball1x,
-            Math.cos(i)*30+ball1y,
-        );
-    }
-    ctx.stroke();
-
-    ball1x=ball1x+15*duration;
-    ball1y=ball1y-17*duration;
-
-    ctx.beginPath();
-    ctx.strokeStyle = "green"
-    for(let i= -Math.PI;i<=Math.PI;i+=0.03){
-        ctx.lineTo(
-            Math.cos(i)*30+ball2x,
-            Math.sin(i)*30+ball2y,
-        );
-    }
-    ctx.stroke();
-
-    ball2x=ball2x-30*duration;
-    ball2y=ball2y+7*duration;
-
-    ctx.beginPath();
-    ctx.strokeStyle = "orange"
-    for(let i= -Math.PI;i<=Math.PI;i+=0.03){
-        ctx.lineTo(
-            Math.cos(i)*30+ball3x,
-            Math.sin(i)*30+ball3y,
-        );
-    }
-    ctx.stroke();
-
-    ball3x=ball3x+30*duration;
-    ball3y=ball3y+0*duration;
-
-    ctx.beginPath();
-    ctx.strokeStyle = "purple"
-    for(let i= -Math.PI;i<=Math.PI;i+=Math.PI*2/5){
-        ctx.lineTo(
-            Math.cos(i)*30+ball4x,
-            Math.sin(i)*30+ball4y,
-        );
-    }
-    ctx.stroke();
-
-    ball4x=ball4x+18*duration;
-    ball4y=ball4y+36*duration;
-
-
     for(let ball of balls){
         ctx.beginPath();
-        ctx.strokeStyle = ball.color||"white"
-        for(let i= -Math.PI;i<=Math.PI;i+=Math.PI*2/8){
+        ctx.strokeStyle = ball.color||"white";
+        const radians = (ball.rotation||0)/180*Math.PI;
+        for(let i= -Math.PI;i<=Math.PI*4;i+=Math.PI*2/(ball.edges||16)){
             ctx.lineTo(
-                Math.cos(i)*30+ball.x,
-                Math.sin(i)*30+ball.y,
+                Math.cos(i+radians)*(ball.width||ball.size||30)+ball.x,
+                Math.sin(i+radians)*(ball.height||ball.size||30)+ball.y,
             );
         }
         ctx.stroke();
@@ -223,11 +160,8 @@ const drawBall = ()=>{
         ball.y=ball.y+(Math.random()-0.5)*100*duration;
     }
 
-    //if(ballX<100){
-        //drawBall();
-        //setTimeout(drawBall,Math.random()*100);
-        requestAnimationFrame(drawBall);
-    //}
+    requestAnimationFrame(drawBall);
+
 };
 drawBall();
 
