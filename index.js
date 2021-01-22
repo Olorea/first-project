b26a600bc48def8e8381f992d3f02562cbd1ca27
Olorea <firchovaa@gmail.com>
2021-01-22 20:27:46 +0100

    Axis x

--- index.js
+++ index.js
@@ -5,6 +5,21 @@ const ctx = canvas.getContext ('2d')
 
 
 ctx.beginPath();
+             
+ctx.lineWidth = "2";
+ctx.strokeStyle = "green";  // Green path/ osa y
+ctx.moveTo(0, 500);
+ctx.lineTo(0, 50);
+ctx.stroke();  // Draw it
+
+ctx.beginPath();
+ctx.lineWidth = "2";
+ctx.strokeStyle = "purple";  // Purple path/osa x
+ctx.moveTo(0, 250);
+ctx.lineTo(500, 250);  
+ctx.stroke();   
+
+ctx.beginPath();
 ctx.strokeStyle = "red";
 for(let x=0;x<=500;x+=1){
     ctx.lineTo(
