8951260b5418a66fac2f899ee38a88828c42bd4a
Olorea <firchovaa@gmail.com>
2021-01-22 20:51:55 +0100

    first graph

--- index.js
+++ index.js
@@ -7,13 +7,17 @@ const ctx = canvas.getContext ('2d')
 ctx.beginPath();
              
 ctx.lineWidth = "2";
+ctx.font = "30px Arial";
+ctx.fillText("y", 260, 70); 
 ctx.strokeStyle = "green";  // Green path/ osa y
-ctx.moveTo(0, 500);
-ctx.lineTo(0, 50);
+ctx.moveTo(250, 500);
+ctx.lineTo(250, 50);
 ctx.stroke();  // Draw it
 
+//====================[ Axis X ]===
 ctx.beginPath();
 ctx.lineWidth = "2";
+ctx.fillText("x", 480, 310);
 ctx.strokeStyle = "purple";  // Purple path/osa x
 ctx.moveTo(0, 250);
 ctx.lineTo(500, 250);  
@@ -21,6 +25,7 @@ ctx.stroke();
 
 ctx.beginPath();
 ctx.strokeStyle = "red";
+
 for(let x=0;x<=500;x+=1){
     ctx.lineTo(
         x,
@@ -28,4 +33,5 @@ for(let x=0;x<=500;x+=1){
     );
 }
 ctx.stroke();
+
 
